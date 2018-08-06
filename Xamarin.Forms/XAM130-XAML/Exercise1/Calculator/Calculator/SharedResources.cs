ee7b9910c2eb7c957102af061f2be43da21abb99
Loryleen Batioco <Loryleen@Outlook.com>
2018-08-06 12:53:42 -0700

    add Xamarin.Forms projects folder

---
+++ Xamarin.Forms/XAM130-XAML/Exercise1/Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    // TODO #10 : 
+        // create shared resources file
+        // create a sample resource
+        // wire up sample resource to MainPage.xaml
+    class SharedResources
+    {
+        // sample resource for Operation button background color
+        public static Color OpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+    }
+}
