bf956a8d69e9deec7429ac92228632bdf59fc122
Loryleen Batioco <Loryleen@Outlook.com>
2018-08-08 15:52:59 -0700

    add XAM160 folder with People exercise

---
+++ Xamarin.Forms/XAM160-SQLite/People/People/People.UWP/FileAccessHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.UWP
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string filename)
+        {
+            // folder location can be found using Windows.Storage.ApplicationData.Current.LocalFolder.Path.
+            string path = global::Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            return System.IO.Path.Combine(path, filename);
+        }
+    }
+}
